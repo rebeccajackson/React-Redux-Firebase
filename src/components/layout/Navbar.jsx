89acb0fd0d162ebbb64c1d99966955b073b4fe9b
Rebecca Jackson <zimbecs@gmail.com>
2019-04-10 11:19:12 +1200

    Navbar setup done

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import SignedInLinks from './SignedInLinks'
+import SignedOutLinks from './SignedOutLinks'
 
 const Navbar = () => {
   return (
     <div>
-      <nav className="nav-wrapper grey-darken-3">
+      <nav className="nav-wrapper grey darken-3">
         <div className='container'>
           <Link to='/' className='brand-logo' > Task Planner </Link>
+          <SignedInLinks />
+          <SignedOutLinks />
         </div>
       </nav>
     </div>
