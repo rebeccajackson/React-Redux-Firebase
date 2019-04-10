99408e860f6e79f7dce16a848e41988177e7b696
Rebecca Jackson <zimbecs@gmail.com>
2019-04-11 10:44:08 +1200

    added props and passed props

--- src/components/projects/ProjectSummary.jsx
+++ src/components/projects/ProjectSummary.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const ProjectSummary = () => {
+const ProjectSummary = ({project}) => {
   return (
     <div className="card z-depth-0 project-summary">
       <div className="card-content grey-text text-darken-3">
-        <span className="car-title">Project Title</span>
+        <span className="car-title">{project.title}</span>
         <p>Posted by Me</p>
         <p className="grey-text"> 10 April 2019, 2am</p>
       </div>
