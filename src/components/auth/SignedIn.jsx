e7902f76abcb372e726b862ae91a98c928078f97
Rebecca Jackson <zimbecs@gmail.com>
2019-04-10 12:26:39 +1200

    updated handleChange and submit functions

--- src/components/auth/SignedIn.jsx
+++ src/components/auth/SignedIn.jsx
@@ -2,15 +2,19 @@ import React, { Component } from 'react'
 
 export class SignedIn extends Component {
   state = {
-     
+    email: '',
+    password: ''
   }
 
   handleChange = (e) => {
-    console.log(e)
+    this.setState({
+      [e.target.id]: e.target.value
+    })
   }
  
   handleSubmit = (e) => {
-    console.log(e)
+    e.preventDefault()
+    console.log(this.state)
   }
 
   render() {
