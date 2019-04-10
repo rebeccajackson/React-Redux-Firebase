e65a5233b48740a420213812bcc17fa061640eb3
Rebecca Jackson <zimbecs@gmail.com>
2019-04-11 11:45:05 +1200

    added createProject dispatched action and reducer

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -7,6 +7,10 @@ const initialState = {
 }
 
 const projectReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'CREATE_PROJECT':
+      console.log('created project', action.project)
+  }
   return state
 }
 
