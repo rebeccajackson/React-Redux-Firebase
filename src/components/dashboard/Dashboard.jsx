f2faff02ac294a556e250ef757ec18a3ff81e632
Rebecca Jackson <zimbecs@gmail.com>
2019-04-11 10:43:33 +1200

    set initial state and connected redux

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,16 +1,18 @@
 import React, { Component } from 'react'
 import Notifications from './Notifications'
 import ProjectList from '../projects/ProjectList'
+import { connect } from 'react-redux'
 
+class Dashboard extends Component {
 
-export default class Dashboard extends Component {
   render() {
+    const { projects } = this.props
     return (
       <div className='dashboard container'>
 
         <div className="row">
           <div className="col s12 m6">
-            <ProjectList />
+            <ProjectList projects={projects}/>
           </div>
           <div className="col s12 m5 offset-m1">
             <Notifications />
@@ -21,3 +23,10 @@ export default class Dashboard extends Component {
     )
   }
 }
+const mapStateToProps = (state) => {
+  return {
+    projects: state.project.projects
+  }
+}
+
+export default connect(mapStateToProps)(Dashboard)
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initialState = {}
+const initialState = {
+  projects: [
+    {id: '1', title: 'plant snowpea seeds', content: 'seeds dirt water'},
+    {id: '2', title: 'clean garage', content: 'broom throw out trash'},
+    {id: '3', title: 'surf on saturday', content: 'togs towel board'}
+  ]
+}
 
 const projectReducer = (state = initialState, action) => {
   return state
