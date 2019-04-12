231cd3bd2ba2a1f840cb349d5cdff6bfdc989ddb
Rebecca Jackson <zimbecs@gmail.com>
2019-04-12 13:00:09 +1200

    connected firebase auth

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import SignedInLinks from './SignedInLinks'
 import SignedOutLinks from './SignedOutLinks'
+import { connect } from 'react-redux'
 
 const Navbar = () => {
   return (
     <div>
       <nav className="nav-wrapper grey darken-3">
         <div className='container'>
-          <Link to='/' className='brand-logo' > Task Planner </Link>
+          <Link to='/' className='brand-logo'> Task Planner </Link>
           <SignedInLinks />
           <SignedOutLinks />
         </div>
@@ -17,4 +18,11 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+const mapStateToProps = (state) => {
+  console.log(state)
+  return {
+
+  }
+}
+
+export default connect(mapStateToProps)(Navbar)
