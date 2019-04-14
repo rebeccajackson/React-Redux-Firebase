a958db364d9c4d1d55fb2a7e037cc0b98d1b7b5e
Rebecca Jackson <zimbecs@gmail.com>
2019-04-15 11:34:23 +1200

    DOM load after authenticated

--- src/index.js
+++ src/index.js
@@ -15,9 +15,13 @@ const store = createStore(rootReducer,
   compose(
     applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
     reduxFirestore(fbConfig),
-    reactReduxFirebase(fbConfig)
+    reactReduxFirebase(fbConfig, {attachAuthIsReady: true})
     )
   )
 
-  ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-  registerServiceWorker();
+  store.firebaseAuthIsReady.then(() => {
+    ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+    registerServiceWorker();
+  })
+
+
