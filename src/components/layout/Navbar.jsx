e23542f66ee34f39485aff135d4a7d1c1ab4634b
Rebecca Jackson <zimbecs@gmail.com>
2019-04-15 11:30:16 +1200

    sign in links conditional

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -4,14 +4,15 @@ import SignedInLinks from './SignedInLinks'
 import SignedOutLinks from './SignedOutLinks'
 import { connect } from 'react-redux'
 
-const Navbar = () => {
+const Navbar = (props) => {
+  const { auth } = props
+  const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />
   return (
     <div>
       <nav className="nav-wrapper grey darken-3">
         <div className='container'>
           <Link to='/' className='brand-logo'> Task Planner </Link>
-          <SignedInLinks />
-          <SignedOutLinks />
+          { links }
         </div>
       </nav>
     </div>
@@ -19,9 +20,8 @@ const Navbar = () => {
 }
 
 const mapStateToProps = (state) => {
-  console.log(state)
   return {
-
+    auth: state.firebase.auth
   }
 }
 
