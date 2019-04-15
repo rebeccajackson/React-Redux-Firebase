d9346719986262ac55c18972b9e4a64cbe1b1c28
Rebecca Jackson <zimbecs@gmail.com>
2019-04-15 12:44:10 +1200

    added moment to format date

--- src/components/projects/ProjectSummary.jsx
+++ src/components/projects/ProjectSummary.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import moment from 'moment'
 
 const ProjectSummary = ({project}) => {
   return (
@@ -6,7 +7,7 @@ const ProjectSummary = ({project}) => {
       <div className="card-content grey-text text-darken-3">
         <span className="car-title">{project.title}</span>
         <p>Posted by {project.authorFirstName} {project.authorLastName}</p>
-        <p className="grey-text"> 10 April 2019, 2am</p>
+        <p className="grey-text"> {moment(project.createdAt.toDate()).calendar()}</p>
       </div>
     </div>
   )
