bc8de0182031e9eb4f336cbbee6ba84120fd3a8c
Rebecca Jackson <zimbecs@gmail.com>
2019-04-15 12:38:33 +1200

    create project with correct author info

--- src/components/projects/CreateProject.jsx
+++ src/components/projects/CreateProject.jsx
@@ -18,6 +18,7 @@ export class CreateProject extends Component {
   handleSubmit = (e) => {
     e.preventDefault()
     this.props.createProject(this.state)
+    this.props.history.push('/')
   }
 
   render() {
--- src/components/projects/ProjectSummary.jsx
+++ src/components/projects/ProjectSummary.jsx
@@ -5,7 +5,7 @@ const ProjectSummary = ({project}) => {
     <div className="card z-depth-0 project-summary">
       <div className="card-content grey-text text-darken-3">
         <span className="car-title">{project.title}</span>
-        <p>Posted by Me</p>
+        <p>Posted by {project.authorFirstName} {project.authorLastName}</p>
         <p className="grey-text"> 10 April 2019, 2am</p>
       </div>
     </div>
