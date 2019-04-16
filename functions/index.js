a8fb07bd0f52dc11dbd49ba00dc532c1491271c2
Rebecca Jackson <zimbecs@gmail.com>
2019-04-17 09:42:52 +1200

    corrected formating

--- functions/index.js
+++ functions/index.js
@@ -8,20 +8,20 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 
 const createNotification = (notification => {
   return admin.firestore().collection('notifications')
-  .add(notification)
-  .then(doc => console.log('notification added', doc))
+    .add(notification)
+    .then(doc => console.log('notification added', doc))
 })
 
 exports.projectCreated = functions.firestore
-.document('projects/{projectId}')
-.onCreate(doc => {
-  
-  const project = doc.data()
-  const notification = {
-    content: 'Added a new project',
-    user: `${project.authorFirstName} ${project.authorLastName}`,
-    time: admin.firestore.FieldValue.serverTimestamp()
-  }
+  .document('projects/{projectId}')
+  .onCreate(doc => {
+    
+    const project = doc.data()
+    const notification = {
+      content: 'Added a new project',
+      user: `${project.authorFirstName} ${project.authorLastName}`,
+      time: admin.firestore.FieldValue.serverTimestamp()
+    }
 
   return createNotification(notification)
 })
